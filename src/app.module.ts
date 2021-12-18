f2db2736cfaafea0fe7c52f3ff28cf50480b8f87
Stefan Meier <slm@meiersolutions.dk>
2021-12-18 07:36:12 +0100

    initial commit with user service

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule} from '@nestjs/typeorm'
+import { GraphQLModule } from '@nestjs/graphql';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'bkmps',
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/users/users.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserInput } from './dto/create-user.input';
+import { UpdateUserInput } from './dto/update-user.input';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectRepository(User) private userRepository: Repository<User>) {}
+  create(createUserInput: CreateUserInput): Promise<User> {
+    const newUser = this.userRepository.create(createUserInput)
+    return this.userRepository.save(newUser)
+  }
+
+  findAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+
+  findOne(id: number): Promise<User> {
+    return this.userRepository.findOneOrFail(id);
+  }
+
+  async update(id: number, updateUserInput: UpdateUserInput,): Promise<User> {
+    const user = await this.userRepository.preload({
+      id: id,
+      ...updateUserInput,
+    });
+    if (!user) {
+      throw new NotFoundException(`User #${id} not found`);
+    }
+    return this.userRepository.save(user);
+  }
+
+  async remove(id: number) {
+    const entity = await this.userRepository.findOneOrFail(id)
+    return await this.userRepository.softRemove(entity)
+  }
+}
