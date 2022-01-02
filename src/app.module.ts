357bb40d459fdb014f66e757ebfaddca0a93a797
Stefan Meier <slm@meiersolutions.dk>
2022-01-02 21:05:30 +0000

    use in memory sqlite database for e2e testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,21 +5,40 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { User } from './users/entities/user.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql',
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'bkmps',
-      entities: [User],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        if (configService.get<string>('NODE_ENV') === 'test') {
+          console.log('Using in memory test database...');
+          return {
+            type: 'sqlite',
+            database: ':memory:',
+            dropSchema: true,
+            synchronize: true,
+            entities: [User],
+            logging: false,
+          };
+        }
+        console.log('Using postgres database...');
+        return {
+          type: 'postgres',
+          host: 'localhost',
+          port: 5432,
+          username: 'postgres',
+          password: 'postgres',
+          database: 'bkmps',
+          entities: [User],
+          synchronize: true,
+        };
+      },
+      inject: [ConfigService],
     }),
     UsersModule,
   ],
